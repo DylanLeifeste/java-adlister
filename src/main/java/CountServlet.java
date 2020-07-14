71b7cb4e2c16b070f6886502878491d6861c86f8
Dylan Leifeste <ldylan01@gmail.com>
2020-07-14 11:51:06 -0500

    Servlets

---
+++ src/main/java/CountServlet.java
@@ -0,0 +1,22 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "CountServlet", urlPatterns = "/count")
+public class CountServlet extends HttpServlet {
+
+
+        int count = 0;
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+         count += 1;
+        out.println("<h1> Count: " + count + "</h1>");
+    }
+
+}
